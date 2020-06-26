0f6633d601495c67bbf865ef1e965439f78350ee
Sarahmdawood <sarahmajeed368@gmail.com>
2020-06-26 15:56:58 +0500

    Adding text boxes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import 'Signin.scss';
+import './Signin.scss';
 
 class Signin extends Component {
   constructor() {
