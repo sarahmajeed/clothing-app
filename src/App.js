54049346232e72fd30492ffc5cc79a84e5706766
Sarahmdawood <sarahmajeed368@gmail.com>
2020-06-26 13:02:44 +0500

    Adding main root route i.e. HomePage in App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* importing homepage route */}
+      <Route exact path="/" render={() => <HomePage />} />
     </div>
   );
 }
