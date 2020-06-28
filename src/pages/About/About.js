a710719fe0644b52b7b60762902a3df8ee6a690f
Sarahmdawood <sarahmajeed368@gmail.com>
2020-06-28 20:13:18 +0500

    Making animated arrows for scroll snap

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,15 +7,34 @@ const About = () => {
       <section className="mission">
         <h1>Our mission</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, eos perspiciatis facere nisi recusandae, porro fugiat ducimus autem odit, cumque obcaecati. Dolorum hic aperiam adipisci eveniet eligendi consectetur accusantium magnam facere maiores delectus placeat ab temporibus neque id, expedita at autem vitae consequatur eius, iste quis ad illo? Adipisci, dolorem.</p>
+        <div className="box">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
       </section>
+
       <section className="promise">
         <h1>Our promise</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum corporis iste nihil fuga magni error consectetur sapiente provident, unde veritatis minima nisi natus nobis facere ratione quisquam, ipsam culpa. Reiciendis consequuntur dolorum iusto fuga atque porro perferendis quod, temporibus officiis? Hic similique fugit reprehenderit, libero suscipit sapiente magnam accusamus incidunt?</p>
+
       </section>
+      <div className="box">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
       <section className="essence">
         <h1>Our essence</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat iusto autem excepturi impedit placeat eveniet. Sit quidem, recusandae ad quae qui aliquid ullam porro iusto alias ipsum eius dolore aspernatur neque rem laborum iste accusamus deserunt illo nisi sunt modi! Impedit dicta commodi voluptates, voluptas sint expedita vero nam! Praesentium.</p>
+
       </section>
+      <div className="box">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   )
 }
