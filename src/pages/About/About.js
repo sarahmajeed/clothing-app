9707e15f46334026dee31c6d71f52a7bb04a95d2
Sarahmdawood <sarahmajeed368@gmail.com>
2020-06-28 16:24:23 +0500

    About page basic structure

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './About.scss';
 
 const About = () => {
   return (
-    <div>
-      <p>About</p>
+    <div className="about">
+      <section className="mission">
+        <h1>Our mission</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, eos perspiciatis facere nisi recusandae, porro fugiat ducimus autem odit, cumque obcaecati. Dolorum hic aperiam adipisci eveniet eligendi consectetur accusantium magnam facere maiores delectus placeat ab temporibus neque id, expedita at autem vitae consequatur eius, iste quis ad illo? Adipisci, dolorem.</p>
+      </section>
+      <section className="promise">
+        <h1>Our promise</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum corporis iste nihil fuga magni error consectetur sapiente provident, unde veritatis minima nisi natus nobis facere ratione quisquam, ipsam culpa. Reiciendis consequuntur dolorum iusto fuga atque porro perferendis quod, temporibus officiis? Hic similique fugit reprehenderit, libero suscipit sapiente magnam accusamus incidunt?</p>
+      </section>
+      <section className="essence">
+        <h1>Our essence</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat iusto autem excepturi impedit placeat eveniet. Sit quidem, recusandae ad quae qui aliquid ullam porro iusto alias ipsum eius dolore aspernatur neque rem laborum iste accusamus deserunt illo nisi sunt modi! Impedit dicta commodi voluptates, voluptas sint expedita vero nam! Praesentium.</p>
+      </section>
     </div>
   )
 }
