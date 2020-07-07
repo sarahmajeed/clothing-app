f073b4b16597389c0bb6874a97dcb87a31d13622
Sarahmdawood <sarahmajeed368@gmail.com>
2020-07-07 15:47:17 +0500

    About page styled and done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import './About.scss';
 
+function scrollUp() {
+  return window.scrollTo(0, 0);
+}
+
+function scrollDown() {
+  let pageHeight = window.innerHeight;
+  console.log('working button');
+  window.scrollBy(0, pageHeight);
+}
+
 const About = () => {
   return (
     <div className="about">
       <section className="mission">
-        <div className="box">
-          <span></span>
-          <span></span>
-          <span></span>
+        <button onClick={scrollDown} className="box">
+          <i class="fas fa-angle-double-down fa-2x"></i>
 
-        </div>
+        </button>
         <h1>Our mission</h1>
         <p>We are on a mission to lead, to enhance and to provide best-value products and services to global customers.To make a difference through our branding to stay ahead of fashion trends, market changes and the latest technology.</p>
       </section>
 
 
       <section className="promise">
-        <div className="box1">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+        <button onClick={scrollDown} className="box1">
+          <i class="fas fa-angle-double-down fa-2x"></i>
+        </button>
         <h1>Our promise</h1>
         <p>We promise to inspire generations with the quality of our brand. And to cultivate more fashion choices that are good for people, the planet and your wallet.</p>
 
@@ -32,16 +38,16 @@ const About = () => {
 
         <h1>Our essence</h1>
 
-        <p>Our print is our essence</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit tempora illum ipsum incidunt possimus corporis odit voluptate ut sit ea, officia aliquam aliquid perspiciatis labore eaque nemo nostrum. Quos, vel.</p>
 
 
       </section>
 
-      <a className="arrow" href="#">
-        <i class="fas fa-angle-up" ></i>
-      </a>
+      <button className="arrow" onClick={scrollUp}>
+        <i class="fas fa-angle-double-up fa-2x" ></i>
+      </button>
 
-    </div>
+    </div >
   )
 }
 
