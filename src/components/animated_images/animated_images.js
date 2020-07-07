886cdeecc26e60e7ae31e5e0dd36bef7fbc7882f
Muhammad Abdullah Hayat <malik_muhammad222@outlook.com>
2020-07-08 01:02:34 +0500

    html code  added

---
+++ src/components/animated_images/animated_images.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const animated_images = () => {
+  return (
+    <div class="component">
+      <a href="#">
+        <div class="collection-component">
+          <div class="imgBox">
+            <img
+              src="https://th.bing.com/th/id/OIP.Z9VBg8-ceivUljpqU_qC9QHaEK?pid=Api&rs=1"
+              alt=""
+              style="width: 300px; height: 300px;"
+            />
+          </div>
+          <div class="details">
+            <div class="content" id="collection">
+              <h2>COLLECTION</h2>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="collection-component">
+          <div class="imgBox">
+            <img
+              src="https://i.pinimg.com/736x/43/13/e4/4313e409ae68150a4fdbb61ffcf1c300--mehndi-dresses-pakistani-dresses.jpg"
+              alt=""
+              style="width: 300px;"
+            />
+          </div>
+          <div class="details">
+            <div class="content">
+              <h2>ABOUT US</h2>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default animated_images;
