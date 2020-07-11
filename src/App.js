efccc479163fb779f2d9ab2b226a05d5d3aa829f
Sarahmdawood <sarahmajeed368@gmail.com>
2020-07-11 23:53:07 +0500

    Changing image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
+
 import Navbar from './components/Navbar/Navbar';
+import Slideshow from './components/Slideshow/Slideshow';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* importing homepage route */}
+      <Slideshow />
+
 
     </div>
   );
--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,39 +1,39 @@
 import React from 'react'
-
 import './Slideshow.scss';
 
-import {slide1} from '../images/1A.jpg'
-import {slide2} from '../images/2A.jpg'
-import {slide3} from '../images/3A.jpg'
-import {slide4} from '../images/9.png'
-const Slides = () => {
+import { slide1 } from '../../images/1A.jpg'
+import { slide2 } from '../../images/2A.jpg'
+import { slide3 } from '../../images/3A.jpg'
+import { slide4 } from '../../images/9.PNG'
+
+const Slideshow = () => {
     //Displays slides
-    function slideshow()
-    {
-         // Array for slides
-         const images = [
-            slide1,
-            slide2,
-            slide3,
-            slide4
-        ]
-    // Initializing index for array elements
-    let i = 0;
-    
-    document.getElementById("slides").src = images[i]; //Storing link of images in img tag
-    if(i < images.length-1) // Checking if index value is less than array length
-    {
-    i++; // incrementing index value
-    } 
-    else{
-        i = 0; // setting index value to zero if i > array length
-    }
-    }
-    setInterval(slideshow, 3000);
+    // function slideshow() {
+    //     // Array for slides
+    //     const images = [
+    //         slide1,
+    //         slide2,
+    //         slide3,
+    //         slide4
+    //     ]
+    //     // Initializing index for array elements
+    //     let i = 0;
+
+    //     document.getElementById("slides").src = images[i]; //Storing link of images in img tag
+    //     if (i < images.length - 1) // Checking if index value is less than array length
+    //     {
+    //         i++; // incrementing index value
+    //     }
+    //     else {
+    //         i = 0; // setting index value to zero if i > array length
+    //     }
+    // }
+    // setInterval(slideshow, 3000);
     return (
-        <div className = "container">
-            <img id = "slides" src = {slide1} alt=""/>
+        <div className="container">
+            {/* <img id="slides" src='../../images/1A.jpg' alt="Error" /> */}
+            <img src='../../images/download.jpg' alt="error" />
         </div>
-    )
+    );
 }
-export default Slides;
+export default Slideshow;
