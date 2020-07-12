8462d7b12b0bf529a1c665dd98576534cb6db132
halahnasir <hnasir77@gmail.com>
2020-07-12 10:48:17 -0700

    Made changes in Slideshow folder

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Slideshow.scss";
 
 import slide1 from "../../images/sarah.jpg";
@@ -6,31 +6,28 @@ import slide2 from "../../images/pic1.png";
 import slide3 from "../../images/sarah.jpg";
 import slide4 from "../../images/pic1.png";
 
-class Slideshow extends Component {
-  //Displays slides
-  slides() {
-    // Array for slides
-    const images = [slide1, slide2, slide3, slide4];
-    // Initializing index for array elements
-    let i = 0;
+import SwiperCore, { Navigation } from 'swiper';
+import { Swiper , SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+SwiperCore.use([Navigation]);
 
-    document.getElementById("slides").src = images[i]; //Storing link of images in img tag
-    if (i < images.length - 1) {
-      // Checking if index value is less than array length
-      i++; // incrementing index value
-    } else {
-      i = 0; // setting index value to zero if i > array length
-    }
-  }
-  //setInterval(slideshow, 3000);
-  render() {
-    return (
-      <div className="container">
-        {/* <img id="slides" src='../../images/1A.jpg' alt="Error" /> */}
-        <img id="slides" src={slide1} alt="error" />
-        {setInterval(this.slides, 3000)}
-      </div>
-    );
-  }
-}
-export default Slideshow;
+
+export default () =>
+{
+  return(
+    <Swiper
+      spaceBetween = {50}
+      slidesPerView = {3}
+      navigation
+      onSwiper = {(swiper) => console.log(swiper)}
+      onSlideChange = {() => console.log('slide change')}
+    >
+    <SwiperSlide><img src={slide1} alt="error1"/></SwiperSlide>
+    <SwiperSlide><img src={slide2} alt="error2"/></SwiperSlide>
+    <SwiperSlide><img src={slide3} alt=""/></SwiperSlide>
+    <SwiperSlide><img src={slide4} alt=""/></SwiperSlide>
+    </Swiper>
+  );
+};
+
