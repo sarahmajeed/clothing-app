5c06f2c5a714997fd2c6dcb2fbf631449e978d53
samanafaq <samanfaq27@gmail.com>
2020-07-13 14:26:10 +0500

    Green signal for Abdullah to work on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
         <h4>
           <a href="/">
             <span className="e">Ǝ</span>
-            <span>-COMPANY</span>
+            <span>COMPANY</span>
           </a>
         </h4>
       </div>
