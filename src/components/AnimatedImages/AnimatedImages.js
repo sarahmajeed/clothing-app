ad2826df67c047d1ec398e94b1c98b72730f0a58
Muhammad Abdullah Hayat <malik_muhammad222@outlook.com>
2020-07-15 01:59:08 +0500

    Aminated images component done

--- src/components/AnimatedImages/AnimatedImages.js
+++ src/components/AnimatedImages/AnimatedImages.js
@@ -1,35 +1,31 @@
 import React from "react";
+import "./AnimatedImages.scss";
+
+import box1 from "../../images/3A.jpg";
+import box2 from "../../images/2A.jpg";
 
 const AnimatedImages = () => {
   return (
-    <div class="component">
+    <div className="animated-images-component">
       <a href="#">
-        <div class="collection-component">
-          <div class="imgBox">
-            <img
-              src="https://th.bing.com/th/id/OIP.Z9VBg8-ceivUljpqU_qC9QHaEK?pid=Api&rs=1"
-              alt=""
-
-            />
+        <div className="collection-component">
+          <div className="imgBox">
+            <img src={box1} alt="" />
           </div>
-          <div class="details">
-            <div class="content" id="collection">
+          <div className="details">
+            <div className="content" id="collection">
               <h2>COLLECTION</h2>
             </div>
           </div>
         </div>
       </a>
-      <a href="#">
-        <div class="collection-component">
-          <div class="imgBox">
-            <img
-              src="https://i.pinimg.com/736x/43/13/e4/4313e409ae68150a4fdbb61ffcf1c300--mehndi-dresses-pakistani-dresses.jpg"
-              alt=""
-
-            />
+      <a href="/about">
+        <div className="collection-component">
+          <div className="imgBox">
+            <img src={box2} alt="" />
           </div>
-          <div class="details">
-            <div class="content">
+          <div className="details">
+            <div className="content">
               <h2>ABOUT US</h2>
             </div>
           </div>
