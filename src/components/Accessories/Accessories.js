b9f99e2d58fd0f3efd9e217ea9392f8b172a7996
Sarahmdawood <sarahmajeed368@gmail.com>
2020-08-13 20:16:22 +0500

    Done with accessories

--- src/components/Accessories/Accessories.js
+++ src/components/Accessories/Accessories.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import AccList from '../AccList/AccList';
+import SearchBox from '../SearchBox/SearchBox'
 
 function Accessories() {
+  const [acc, setAcc] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/accessories', {
+      method: 'get',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        setAcc(res)
+      })
+  }, [])
   return (
     <div>
-      Accessories
+      <SearchBox search={search} setSearch={setSearch} />
+      <AccList search={search} acc={acc} setAcc={setAcc} />
     </div>
   )
 }
