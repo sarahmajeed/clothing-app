66550d68893a2e68aa14da2fecee14813cad8919
Sarahmdawood <sarahmajeed368@gmail.com>
2020-09-08 16:01:32 +0500

    About us page animation

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,7 +5,23 @@ function scrollUp() {
   return window.scrollTo(0, 0);
 }
 
-function scrollDown() {
+function scrollDownPromise() {
+  document.getElementById("our-promise").classList.add("final-animation-h1");
+  document
+    .getElementById("our-promise-para")
+    .classList.add("final-animation-para");
+
+  let pageHeight = window.innerHeight;
+  console.log("working button");
+  window.scrollBy(0, pageHeight);
+}
+
+function scrollDownEssence() {
+  document.getElementById("our-essence").classList.add("final-animation-h1");
+  document
+    .getElementById("our-essence-para")
+    .classList.add("final-animation-para");
+
   let pageHeight = window.innerHeight;
   console.log("working button");
   window.scrollBy(0, pageHeight);
@@ -15,11 +31,11 @@ const About = () => {
   return (
     <div className="about">
       <section className="mission">
-        <button onClick={scrollDown} className="box">
+        <button onClick={scrollDownPromise} className="box">
           <i class="fas fa-angle-double-down fa-2x"></i>
         </button>
-        <h1>Our mission</h1>
-        <p>
+        <h1 className="our-mission">Our mission</h1>
+        <p className="our-mission-para">
           We are on a mission to lead, to enhance and to provide best-value
           products and services to global customers.To make a difference through
           our branding to stay ahead of fashion trends, market changes and the
@@ -28,11 +44,11 @@ const About = () => {
       </section>
 
       <section className="promise">
-        <button onClick={scrollDown} className="box1">
+        <button onClick={scrollDownEssence} className="box1">
           <i class="fas fa-angle-double-down fa-2x"></i>
         </button>
-        <h1>Our promise</h1>
-        <p>
+        <h1 id="our-promise">Our promise</h1>
+        <p id="our-promise-para">
           We promise to inspire generations with the quality of our brand. And
           to cultivate more fashion choices that are good for people, the planet
           and your wallet.
@@ -40,9 +56,9 @@ const About = () => {
       </section>
 
       <section className="essence">
-        <h1>Our essence</h1>
+        <h1 id="our-essence">Our essence</h1>
 
-        <p>
+        <p id="our-essence-para">
           It's essence is deeply rooted with minimal design and subtle patterns.
         </p>
       </section>
